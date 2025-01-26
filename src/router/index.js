5040bc8690522acab289473b42ed4f0994cde139
zmabic02 <mopo3208@gmail.com>
2025-01-26 10:50:52 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: "/",
+      name: "default",
+      component: HomeView,
+    },
+    {
       path: "/home",
       name: "home",
       component: HomeView,
